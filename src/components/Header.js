ba05d7d29a21fa9991aa5705a5f15cfc9d56dfcf
Nidal Mohammed <nidalmo23@gmail.com>
2023-01-03 23:55:31 +0530

    For mobile Screen

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+      <header className="header">
+          <img 
+              src="../public/troll-face.png" 
+              className="header--image"
+          />
+          <h2 className="header--title">Meme Generator</h2>
+          
+      </header>
+  )
+}
+
+export default Header
